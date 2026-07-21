0c4bac6cb1d5bfec79a5ef2536a4665609c10944
sydomori <sydneyomori@gmail.com>
2026-07-21 19:23:16 +0300

    join employees with offices

--- main.py
+++ main.py
@@ -2,3 +2,14 @@ import sqlite3
 import pandas as pd
 
 conn = sqlite3.connect('data.sqlite')
+
+#employees and their offices
+q = """
+ SELECT firstName, lastName, city, state
+ FROM employees
+ JOIN offices ON employees.officeCode = offices.officeCode
+ ORDER BY firstName, lastName
+"""
+
+df = pd.read_sql_query(q, conn)
+print(df)
